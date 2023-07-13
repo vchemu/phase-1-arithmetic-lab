6a2a36b1bd1a84f10c185c50d94b3a5e31d9e96c
valerine <valerine.chemutai@student.moringaschool.com>
2023-07-13 04:19:13 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 
-// Write your code here
+function multiply(num1 * num2) {
+  return num1 * num2;
+}
+
+console.log(multiply(2,31));
+
+function random() {
+  let x = Math.random();
+}
+console.log(x);
+
+function mod(num3 % num4) {
+  return num3 % num4;
+}
+
+console.log(mod(16,6));
+
+function max(num5, num6){
+
+  if (num5 > num6) {
+    return num5;
+  }
+  return num6;
+}
+
+console.log(max(15, 20));
